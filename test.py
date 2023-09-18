2842ee71d3b32daaf2195f4fa182e00eaae3d52d
Kinuthiaigiria <allanigiria@gmail.com>
2023-09-18 20:03:13 +0300

    commiting a yml file

---
+++ test.py
@@ -0,0 +1,12 @@
+print("Hello world!!")
+numb=int(input("Enter a number"))
+numb2= int(input("Enter another number"))
+
+total = numb + numb2
+
+print(total)
+
+if numb % 2 == 0:
+    print(f"The number {total} is even")
+else:
+    print(f"The number {total} is odd")
